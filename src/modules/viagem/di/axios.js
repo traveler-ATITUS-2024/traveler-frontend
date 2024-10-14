1a1a3041801752cd56293800c936b3048d550be2
Gabriel <gabriel@webcgi.com.br>
2024-10-14 15:32:31 -0300

    :sparkles: feat(viagem): Troca a baseURL

--- src/modules/viagem/di/axios.js
+++ src/modules/viagem/di/axios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 import { getToken } from "../../../core/domain/model/jwtToken";
 
 const axiosInstance = axios.create({
-  // baseURL: "https://traveler-api-n420.onrender.com",
-  baseURL: "http://192.168.200.78:8080",
+  baseURL: "https://traveler-api-n420.onrender.com",
+  // baseURL: "http://192.168.200.78:8080",
   headers: {
     "Content-Type": "application/json",
   },
